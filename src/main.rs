516abf7604f97a0d1cbde65a9821cb7dd1db1a32
Edward Evans <elevans2@wisc.edu>
2025-11-06 15:45:14 -0600

    Enable basic CNN features and add simplest wgpu example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use burn::prelude::*;
+use burn::backend::Wgpu;
+
+type Backend = Wgpu;
+
 fn main() {
-    println!("Hello, world!");
+    let device = Default::default();
+    // create two tensors,
+    // "a": from an array
+    // "b": full of ones
+    let tensor_a = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &device);
+    let tensor_b = Tensor::<Backend, 2>::ones_like(&tensor_a);
+
+    println!("[INFO] result: {}", tensor_a + tensor_b);
 }
