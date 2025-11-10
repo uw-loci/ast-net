2f0359ce532d4856b57a44596aaed06d1931b2c8
Edward Evans <elevans2@wisc.edu>
2025-11-10 11:07:55 -0600

    Add pre-trained stardist models in bincode format
    This commit adds the stardist 2d fixed (512, 512)
    model with its weights in bincode format. The model
    itself was converted into Rust with burn-import.
    The main() here loads this pretrained model and
    a custom one from the burn tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-mod model;
+pub mod models;
 
-use burn::prelude::*;
 use burn::backend::Wgpu;
+use burn_ndarray::NdArray;
 
-use crate::model::ModelConfig;
+use crate::models::stardist::Model;
+use crate::models::test_model::ModelConfig;
 
 type Backend = Wgpu<f32, i32>;
 
 fn main() {
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<Backend>(&device);
+    // load a pretrained model
+    let stardist_model: Model<NdArray<f32>> = Model::default();
+    println!("[DEBUG] fixed stardist model info: \n{}", stardist_model);
 
-    println!("[DEBUG] model info: {}", model);
+    // create a test custom model
+    let device = Default::default();
+    let custom_model = ModelConfig::new(10, 512).init::<Backend>(&device);
+    println!("[DEBUG] custom model info: \n{}", custom_model);
 }
---
+++ src/models/mod.rs
@@ -0,0 +1,4 @@
+pub mod stardist {
+        include!(concat!(env!("CARGO_MANIFEST_DIR"), "/pretrained_models/stardist_2d_dsb2018_fixed.rs"));
+}
+pub mod test_model;
