efee2c2599f35d77c916a35df7d61b879467216c
Edward Evans <elevans2@wisc.edu>
2025-11-12 15:11:04 -0600

    Copy submodule import from imgal

--- src/networks.rs
+++ src/networks.rs
@@ -1,3 +1,5 @@
+use std::ffi::CString;
+
 use burn::backend::Wgpu;
 use burn::prelude::*;
 use ndarray::{Array2, Array3, ArrayView2};
@@ -22,6 +24,7 @@ pub fn register_stardist_module(parent_module: &Bound<'_, PyModule>) -> PyResult
         py_stardist_2d,
         &stardist_module
     )?)?;
+    py_import_module("stardist");
 
     parent_module.add_submodule(&stardist_module)
 }
@@ -57,4 +60,26 @@ fn run_stardist_2d(data: ArrayView2<f32>) -> (Array2<f32>, Array3<f32>) {
     let arr_b = Array3::from_shape_vec((256, 256, 32), result_b).expect("Tensor data reshape failed.");
 
     (arr_a, arr_b)
+}
+
+/// Add a child module to Python's sys.modules dict.
+///
+/// # Description
+///
+/// This function manually adds a given module to Python's sys.modules
+/// dict. This enables imports like `import ast_net.stardist as star`.
+///
+/// # Arguments
+///
+/// * `module_name` - The name of the module to add to sys.modules.
+fn py_import_module(module_name: &str) {
+    let import_cmd = format!(
+        "import sys; sys.modules['imgal.{}'] = '{}'",
+        module_name, module_name
+    );
+    let c_str_cmd =
+        CString::new(import_cmd).expect("Failed to create 'CString' module import command.");
+    Python::attach(|py| {
+        py.run(c_str_cmd.as_c_str(), None, None).unwrap();
+    });
 }
